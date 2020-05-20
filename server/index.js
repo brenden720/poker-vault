b91d6a9b78561aa1d8e9532c721210d75c65abef
Brenden Pennington <brenden.pennington@gmail.com>
2020-05-19 18:49:20 -0700

    Added cors headed for cross-origin

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ const path = require('path');
 require('dotenv').config({
   path: path.join(__dirname, '../.env'),
 });
+const cors = require('cors');
 const express = require('express');
 const apiRouter = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '../client/public')));
 app.use('/api', apiRouter);
